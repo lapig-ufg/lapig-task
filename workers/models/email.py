b04c53cd5961fadc37891c39fbe60cbfc4ff3fa8
Jairo Matos Da Rocha <jairo@geocodelabs.com>
2024-09-30 17:23:52 -0300

    base email

--- workers/models/email.py
+++ workers/models/email.py
@@ -1,4 +1,15 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class EmailResponse(BaseModel):
+    title: str
+    hello : Optional[str]
+    body: str
+    url: Optional[str]
+    regards: Optional[str]
+    team: str
+
+
 class MessageHTML(BaseModel):
     template_name: str
     content: dict
